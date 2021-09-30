d248ea37d7546c83b3b0ba253b2265d69aee1641
mehedi <millathasan125@gmail.com>
2021-09-30 18:48:53 +0600

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import SideMenu from '../Components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <SideMenu></SideMenu>
+      <div>Here We go...</div>
+    </>
+  );
+}
+
+export default App;
